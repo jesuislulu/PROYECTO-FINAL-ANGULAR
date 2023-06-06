057be44df4e5f0cdffdde93f24cc8134b3e71819
Lucia Rocha <luciarocha@imacdelu.local>
2023-06-06 03:10:25 -0300

    colores

--- src/app/auth/mocks/auth-service.mock.ts
+++ src/app/auth/mocks/auth-service.mock.ts
@@ -3,28 +3,43 @@ import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Login } from "src/app/core/models/Login";
 
 export const USER_MOCK: Usuario = {
-    id: 1,
-    firstName: 'Lucia',
-    lastName: 'Rocha',
-    email: 'lucia@mail.com',
-    password: 'lulu00',
-    token: 'hjsdfJHFDSAFMsaDsan12sdanjjadashj3FDBEWH',
-    rol: 'admin'
-}
-  
+  id: 1,
+  firstName: 'Lucia',
+  lastName: 'Rocha',
+  email: 'lucia@mail.com',
+  password: 'lulu00',
+  token: generateToken(), 
+  rol: 'admin'
+};
+
 export class AuthServiceMock {
-  
+
   private authUser$ = new BehaviorSubject<Usuario | null>(null);
 
   login(formValue: Login): void {
+    console.log('Token almacenado:', USER_MOCK.token);
     this.authUser$.next(USER_MOCK);
   }
 
   verificarToken(): Observable<boolean> {
+    const token = USER_MOCK.token;
+    console.log('Token recuperado:', token);
     return of(true);
   }
 
   register(user: Usuario): void {
-    console.log('Prueba de usuario registrado')
+    console.log('Prueba de usuario registrado');
   }
+}
+
+function generateToken(): string {
+  let cadena = '';
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < 40; i++) {
+    const indice = Math.floor(Math.random() * caracteres.length);
+    cadena += caracteres.charAt(indice);
+  }
+
+  return cadena;
 }
